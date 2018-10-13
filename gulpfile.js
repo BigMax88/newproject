602c127c49d21ceebcdb126bb61b8ad4d3a4289b
max <1960276917@qq.com>
2018-10-13 12:03:52 +0800

    这是第三版，完成了三个子页面

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,19 @@
 var gulp = require("gulp");
-gulp.task("copy-html",function(){
+gulp.task("copy-index",function(){
 	gulp.src("index.html")
+	.pipe(gulp.dest("D:\\phpStudy\\WWW\\project"));
+});
+gulp.task("copy-second",function(){
+	gulp.src("second.html")
+	.pipe(gulp.dest("D:\\phpStudy\\WWW\\project"));
+});
+gulp.task("copy-third",function(){
+	gulp.src("third.html")
+	.pipe(gulp.dest("D:\\phpStudy\\WWW\\project"));
+});
+
+gulp.task("copy-fifth",function(){
+	gulp.src("fifth.html")
 	.pipe(gulp.dest("D:\\phpStudy\\WWW\\project"));
 });
 
@@ -18,9 +31,12 @@ gulp.task("img",function(){
 });
 
 gulp.task("watch",function(){
-	gulp.watch("index.html",["copy-html"]);
-	gulp.watch("css/**/*.{jpg,png}",["css"]);
+	gulp.watch(["index.html"],["copy-index"]);
+	gulp.watch("second.html",["copy-second"]);
+	gulp.watch("third.html",["copy-third"]);
+	gulp.watch("fifth.html",["copy-fifth"]);
+	gulp.watch("css/**/*",["css"]);
 	gulp.watch("js/**/*",["js"]);
-	gulp.watch("img/**/*",["img"]);
+	gulp.watch("img/**/*.{jpg,png}",["img"]);
 });
 
